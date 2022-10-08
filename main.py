9d3edb1f21b9c22d03996c49804af4332f7b6049
Caregor <caregor@gmail.com>
2022-10-08 17:10:16 +0300

    Task 2 is done.

--- main.py
+++ main.py
@@ -22,9 +22,20 @@ Sample Output:
 19
 """
 # Task 1
-
-list_a = [1, 4, 6, 79, 3, 5, 6, 8, 8, 9, 2]
-list_b = [1, 4, 56, 7, 8, 9, 5, 4, 3, 789]
-
-print(set(list_a).intersection(set(list_b)))
-
+#
+# list_a = [1, 4, 6, 79, 3, 5, 6, 8, 8, 9, 2]
+# list_b = [1, 4, 56, 7, 8, 9, 5, 4, 3, 789]
+#
+# print(set(list_a).intersection(set(list_b)))
+#
+# Task 2
+#
+# s = '1 2 3 1 4 5 6 7 8 9 0 1 2 2 1'
+# list_numbers = list(map(int, s.split()))
+# store_numbers = []
+# for item in list_numbers:
+#     if item not in store_numbers:
+#         store_numbers.append(item)
+#         print('NO')
+#     else:
+#         print('YES')
